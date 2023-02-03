ab22d72430eb11f1265b85253092a11a8b4347d7
Flor-Arando <arando.florencia@gmail.com>
2023-02-03 00:09:11 -0300

    Only orders with services are shown

--- src/main/java/com/example/crisalisbackend/repository/OrderRepository.java
+++ src/main/java/com/example/crisalisbackend/repository/OrderRepository.java
@@ -1,12 +1,9 @@
 package com.example.crisalisbackend.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.crisalisbackend.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-    public List<Order> findByOrderServices/*NotNull*/();
     
 }
